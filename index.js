b868c787b53e5b75ac0be080862e5e303c03c79d
BHyeonKim <46583212+BHyeonKim@users.noreply.github.com>
2020-01-31 17:20:35 +0900

    building

---
+++ index.js
@@ -0,0 +1,35 @@
+// You're gonna need this
+const NINE_HOURS_MILLISECONDS = 32400000;
+const DAY = 24;
+const HOUR = 60;
+const MINUTE = 60;
+const MILLISECOND = 1000;
+
+function unTransMill(date, hours, minutes, seconds, milliseconds,xmasDay) {
+  const dDay = xmasDay - date;
+  const dDate = Math.floor(dDay/(DAY*HOUR*MINUTE*MILLISECOND));
+  const dHour = Math.floor((dDay%(DAY*HOUR*MINUTE*MILLISECOND))/(HOUR*MINUTE*MILLISECOND));
+  const dMinute = Math.floor(((dDay%(DAY*HOUR*MINUTE*MILLISECOND))%(HOUR*MINUTE*MILLISECOND))/(MINUTE*MILLISECOND));
+  const dSecond = Math.floor((((dDay%(DAY*HOUR*MINUTE*MILLISECOND))%(HOUR*MINUTE*MILLISECOND))%(MINUTE*MILLISECOND))/MINUTE);
+  console.log(`${dDate}days : ${dHour}hours : ${dMinute}minutes : ${dSecond}seconds remains`);
+};
+
+
+function getTime() {
+  // Don't delete this.
+  const xmasDay = new Date("2020-12-24:00:00:00+0900");
+  const date = new Date();
+  unTransMill(
+    date,
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds(),
+    xmasDay
+  );
+}
+
+function init() {
+  setInterval(getTime(),10);
+}
+init();
